d3ebe9f76da6d91ccfe2c0d05ba5605b17e4d68b
cubo142 <doducminh8694@gmail.com>
2020-10-12 15:15:34 +0700

    drawer navigation

--- app/src/main/java/com/example/tudien/MainActivity.java
+++ app/src/main/java/com/example/tudien/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.tudien;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); //set Toolbar thành Actionbar (có thể đặt tên, đặt option menu vào toolbar)
+
     }
+
 }
