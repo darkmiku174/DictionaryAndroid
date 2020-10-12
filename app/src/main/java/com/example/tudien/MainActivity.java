1ba59509db541dbbd658e9c291c13d1d635cf921
cubo142 <doducminh8694@gmail.com>
2020-10-12 15:46:22 +0700

    them button navagation view

--- app/src/main/java/com/example/tudien/MainActivity.java
+++ app/src/main/java/com/example/tudien/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.tudien;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +18,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar); //set Toolbar thành Actionbar (có thể đặt tên, đặt option menu vào toolbar)
-
+        drawer=findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawer,toolbar,
+        R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
+    @Override
+    public void onBackPressed() //hàm tạo ra khi nhấn Back: màng hình chỉ tắt Navagation View,Drawer chứ ko tắt Activity
+    {
+        if(drawer.isDrawerOpen(GravityCompat.START)) //START = bên trái màng hình
+        {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else //Đóng app như bình thường
+            {
+            super.onBackPressed();
+            }
+    }
 }
