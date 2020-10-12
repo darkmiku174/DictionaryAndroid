fddbd64728f7d9fa39a75f56f3c38c11c5dbb45e
cubo142 <doducminh8694@gmail.com>
2020-10-12 16:52:58 +0700

    them note vao code navigation

--- app/src/main/java/com/example/tudien/MainActivity.java
+++ app/src/main/java/com/example/tudien/MainActivity.java
@@ -10,31 +10,34 @@ import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout drawer;
+    private DrawerLayout drawer; //tạo 1 biến thành viên cho Drawer Layout
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar); //Khai báo mình sử dụng Toolbar as Actionbar
         setSupportActionBar(toolbar); //set Toolbar thành Actionbar (có thể đặt tên, đặt option menu vào toolbar)
-        drawer=findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawer,toolbar,
-        R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        //ActionBarDrawerToggle dùng để tạo nút "burger icon"
+        //ActionBarDrawerToggle cần phải khởi tạo 2 biến string trong "res->values->strings.xm để sử sử dụng 2 giá trị này như 1 kiểu input
+        //ActionBarDrawerToggle(context, drawer layout variable, toolbar variable,string 1 variable, string 2 variable)
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        //đéo hiểu cho lắm
     }
 
     @Override
-    public void onBackPressed() //hàm tạo ra khi nhấn Back: màng hình chỉ tắt Navagation View,Drawer chứ ko tắt Activity
+    public void onBackPressed() //hàm tạo ra khi nhấn Back: màng hình chỉ tắt Navagation Drawer,View chứ ko tắt Activity
     {
-        if(drawer.isDrawerOpen(GravityCompat.START)) //START = bên trái màng hình
+        if (drawer.isDrawerOpen(GravityCompat.START)) //START = bên trái màng hình
         {
             drawer.closeDrawer(GravityCompat.START);
-        }
-        else //Đóng app như bình thường
-            {
+        } else //else đóng app như bình thường
+        {
             super.onBackPressed();
-            }
+        }
     }
 }
