9d1b31265c5ec6685c27c4aff869d80e5f680e56
cubo142 <doducminh8694@gmail.com>
2020-10-20 12:40:50 +0700

    bat su kien item trong navigation view

--- app/src/main/java/com/example/tudien/EnEnFragment.java
+++ app/src/main/java/com/example/tudien/EnEnFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,6 +13,7 @@ public class EnEnFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_enen,container,false);
+
     }
 }
 //Inflater là 1 component giúp chuyển layout file (xml) thành View(Java  Code) trong Android
--- app/src/main/java/com/example/tudien/MainActivity.java
+++ app/src/main/java/com/example/tudien/MainActivity.java
@@ -7,7 +7,9 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.core.view.MenuItemCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,91 +25,62 @@ import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private DrawerLayout drawer; //tạo 1 biến thành viên cho Drawer Layout
-
-    ListView listview;
-    ArrayList<String> stringArrayList = new ArrayList<>();
-    ArrayAdapter<String> adapter;
-
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+    private DrawerLayout drawer;                                                                    //tạo 1 biến thành viên cho Drawer Layout
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        /*listview = findViewById(R.id.list_view);
-        for (int i = 0; i <= 100; i++)
-        {
-            stringArrayList.add("Item " + i);
-        }
-
-        adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, stringArrayList);
-        //tạo adapter
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), adapter.getItem(position), Toast.LENGTH_LONG).show();
-            }
-        }); */
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar); //Khai báo mình sử dụng Toolbar as Actionbar
-        setSupportActionBar(toolbar); //set Toolbar thành Actionbar (có thể đặt tên, đặt option menu vào toolbar)
-        drawer = findViewById(R.id.drawer_layout);
 
-        NavigationView navigationView = findViewById(R.id.nav_view); //đéo hiểu cho lắm NOTE
-        navigationView.setNavigationItemSelectedListener(this); //đéo hiểu cho lắm
+        Toolbar toolbar = findViewById(R.id.toolbar);                                                //Khai báo mình sử dụng Toolbar as Actionbar
+        setSupportActionBar(toolbar);                                                                //set Toolbar thành Actionbar (có thể đặt tên, đặt option menu vào toolbar)
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);         //xem lại
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        //ActionBarDrawerToggle dùng để tạo nút "burger icon"
-        //ActionBarDrawerToggle cần phải khởi tạo 2 biến string trong "res->values->strings.xm để sử sử dụng 2 giá trị này như 1 kiểu input
-        //ActionBarDrawerToggle(context, drawer layout variable, toolbar variable,string 1 variable, string 2 variable)
+
         drawer.addDrawerListener(toggle);
         toggle.syncState(); //xem lại
 
-        if(savedInstanceState==null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new EnEnFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_ENEN); //xem lại phần này,Navigation Drawer with fragment
-        }
     }
+    //ActionBarDrawerToggle dùng để tạo nút "burger icon"
+    //ActionBarDrawerToggle cần phải khởi tạo 2 biến string trong "res->values->strings.xml" để sử sử dụng 2 giá trị này như 1 kiểu input
+    // ActionBarDrawerToggle(context, drawer layout variable, toolbar variable,string 1 variable, string 2 variable)
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) { //bắt sự kiện trên navigation View)
+
         switch (item.getItemId()) {
             case R.id.nav_ENEN:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new EnEnFragment()).commit();
+                        new EnEnFragment()).addToBackStack(null).commit();                            //addToBackStack : added fragment -> removed replaced fragment -> restored removed fragment -> added.
                 break;
             case R.id.nav_ENtoVN:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new ENtoVNFragment()).commit();
+                        new ENtoVNFragment()).addToBackStack(null).commit();
                 break;
             case R.id.nav_VNtoEN:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new VNtoENFragment()).commit();
+                        new VNtoENFragment()).addToBackStack(null).commit();
                 break;
             case R.id.nav_FavWord:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new FavWordFragment()).commit();
+                        new FavWordFragment()).addToBackStack(null).commit();
                 break;
         }
+
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-    /* @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //create menu inflater
-        MenuInflater menuInflater = getMenuInflater();
-        //Inflater menu
-        menuInflater.inflate(R.menu.menu_search, menu);
-        //Tạo menu item
-        return super.onCreateOptionsMenu(menu);
-    }*/
-
     @Override
-    public void onBackPressed() //hàm tạo ra khi nhấn Back: màng hình chỉ tắt Navagation Drawer,View chứ ko tắt Activity
+    public void onBackPressed()                                                                      //hàm tạo ra khi nhấn Back: màng hình chỉ tắt Navagation Drawer,View chứ ko tắt Activity
     {
-        if (drawer.isDrawerOpen(GravityCompat.START)) //START = bên trái màng hình
+        FragmentManager manager = getSupportFragmentManager();
+        if (drawer.isDrawerOpen(GravityCompat.START))                                                //START = bên trái màng hình
         {
             drawer.closeDrawer(GravityCompat.START);
         } else //else đóng app như bình thường
@@ -115,5 +88,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
 
+        return true;
+    }
 }
+//https://viblo.asia/p/translation-cac-khai-niem-co-ban-ve-fragment-trong-android-phan-2-GrLZDAQwlk0
+//https://viblo.asia/p/fragment-va-co-che-backstack-va-su-dung-fragment-hieu-qua-nhat-p1-1Je5EMmG5nL
+//developer.android.com/training/search/setup#:~:text=To%20add%20a%20SearchView%20widget,the%20title%20of%20the%20item
